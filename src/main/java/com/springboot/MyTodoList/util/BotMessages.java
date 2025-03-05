4951f7e8ad5720cdd35add8e14e05090ca0665d3
ElingeMisa <a01741176@tec.mx>
2025-03-05 10:03:02 -0600

    Añadir nuevo comando y mensaje al bot, actualizar registros y eliminar archivos obsoletos

--- src/main/java/com/springboot/MyTodoList/util/BotMessages.java
+++ src/main/java/com/springboot/MyTodoList/util/BotMessages.java
@@ -3,18 +3,17 @@ package com.springboot.MyTodoList.util;
 public enum BotMessages {
 	
 	HELLO_MYTODO_BOT(
-	"¡Hola! Soy el Bot de MyTodoList.\nEscribe un nuevo elemento de la lista de tareas a continuación y presiona el botón de enviar (flecha azul), o selecciona una opción a continuación:"),
-	BOT_REGISTERED_STARTED("¡Bot registrado y iniciado con éxito!"),
-	ITEM_DONE("¡Elemento completado! Selecciona /todolist para volver a la lista de tareas, o /start para ir a la pantalla principal."), 
-	ITEM_UNDONE("¡Elemento desmarcado! Selecciona /todolist para volver a la lista de tareas, o /start para ir a la pantalla principal."), 
-	ITEM_DELETED("¡Elemento eliminado! Selecciona /todolist para volver a la lista de tareas, o /start para ir a la pantalla principal."),
-	TYPE_NEW_TODO_ITEM("Escribe un nuevo elemento de la lista de tareas a continuación y presiona el botón de enviar (flecha azul) en el lado derecho."),
-	NEW_ITEM_ADDED("¡Nuevo elemento añadido! Selecciona /todolist para volver a la lista de tareas, o /start para ir a la pantalla principal."),
-	BYE("¡Adiós! Selecciona /start para continuar."),
-	NO_ITEMS("¡No hay elementos para mostrar! Selecciona /start para ir a la pantalla principal."),
-	NO_ITEMS_TO_DELETE("¡No hay elementos para eliminar! Selecciona /start para ir a la pantalla principal."),
-	NEW_HELLO("¡Hola, soy un mensaje nuevo creado por el desarrollador!"),
-	INVALID_COMMAND("¡Por favor, ingrese un comando válido!");
+	"Hello! I'm MyTodoList Bot!\nType a new todo item below and press the send button (blue arrow), or select an option below:"),
+	BOT_REGISTERED_STARTED("Bot registered and started succesfully!"),
+	ITEM_DONE("Item done! Select /todolist to return to the list of todo items, or /start to go to the main screen."), 
+	ITEM_UNDONE("Item undone! Select /todolist to return to the list of todo items, or /start to go to the main screen."), 
+	ITEM_DELETED("Item deleted! Select /todolist to return to the list of todo items, or /start to go to the main screen."),
+	TYPE_NEW_TODO_ITEM("Type a new todo item below and press the send button (blue arrow) on the rigth-hand side."),
+	NEW_ITEM_ADDED("New item added! Select /todolist to return to the list of todo items, or /start to go to the main screen."),
+	BYE("Bye! Select /start to resume!"),
+	NO_ITEMS("No items to show! Select /start to go to the main screen."),
+	NO_ITEMS_TO_DELETE("No items to delete! Select /start to go to the main screen."),
+	NEW_HELLO("Hola, soy un mensaje nuevo creado por el desarrollador!");
 
 	private String message;
 
