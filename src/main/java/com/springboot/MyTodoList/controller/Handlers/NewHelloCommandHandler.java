e1246ea5c39f06dab1f92ee4812b13c57bd05e55
ElingeMisa <a01741176@tec.mx>
2025-03-06 12:50:55 -0600

    primerservicio

--- src/main/java/com/springboot/MyTodoList/controller/Handlers/NewHelloCommandHandler.java
+++ src/main/java/com/springboot/MyTodoList/controller/Handlers/NewHelloCommandHandler.java
@@ -7,6 +7,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import com.springboot.MyTodoList.controller.UsuariosService;
 import com.springboot.MyTodoList.util.BotCommands;
 import com.springboot.MyTodoList.util.BotLabels;
 import com.springboot.MyTodoList.util.BotMessages;
@@ -16,6 +17,8 @@ import com.springboot.MyTodoList.util.BotMessages;
  */
 public class NewHelloCommandHandler implements CommandHandler {
     private static final Logger logger = LoggerFactory.getLogger(NewHelloCommandHandler.class);
+
+    private final UsuariosService usuariosService = null;
 
     @Override
     public boolean canHandle(String messageText) {
