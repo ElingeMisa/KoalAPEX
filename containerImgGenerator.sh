1a54a1751dbe32c846bd14e149182441a9aaef2d
ElingeMisa <a01741176@tec.mx>
2025-03-10 11:42:17 -0600

    Cambios para correr en mac

--- containerImgGenerator.sh
+++ containerImgGenerator.sh
@@ -1,10 +1,9 @@
-docker stop microservice-demo
-docker rm -f microservice-demo
-docker rmi microservice-demo
-mvn verify
-docker build -f Dockerfile --platform linux/amd64 -t <repo_img>:0.1 .  
-docker run --name agilecontainer -p 8080:8080 -d <repo_img>:0.1
-
+docker stop agileOrganizerContainer
+docker rm -f agileOrganizerContainer
+docker rmi agileOrganizerContainer
+call mvn verify
+docker build -f Dockerfile --platform linux/amd64 -t mx-queretaro-1.ocir.io/ax3ubzsr6auc/reacttodo/7kex2/todolistapp-springboot:0.6 .
+docker run -it --name agileOrganizerContainer -p 8080:8080 mx-queretaro-1.ocir.io/ax3ubzsr6auc/reacttodo/7kex2/todolistapp-springboot:0.6
 # ax3ubzsr6auc
 # docker login -u ax3ubzsr6auc/a00835693@tec.mx mx-queretaro-1.ocir.io 
 # D5E>6F8n)+n1:ZnW3{t;  
